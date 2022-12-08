f264955a623764b1d9433831fa063079c86b47d5
Vincent C. Mader <vincent.mader@icloud.com>
2022-12-08 23:23:16 +0100

    fixed arguments to `subprocess.Popen` (must be arg-list)

--- src/p1_initial_server_setup/src/main.py
+++ src/p1_initial_server_setup/src/main.py
@@ -5,7 +5,7 @@ def create_new_user():
     cprint("Creating new user...", indent=1)
     cprint("Username: ", end="", indent=1)
     username = input()
-    execute(f"useradd {username}")
+    execute(["useradd", username])
 
 if __name__ == "__main__":
     cprint("Part 1: Initial Server Setup", color="blue", newline=True)
