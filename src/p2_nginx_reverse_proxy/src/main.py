8ce71118a1d46669087ec10d2c37bde56592cedb
Vincent C. Mader <vincent.mader@icloud.com>
2022-12-08 23:49:13 +0100

    added firewall setup for nginx

--- src/p2_nginx_reverse_proxy/src/main.py
+++ src/p2_nginx_reverse_proxy/src/main.py
@@ -6,7 +6,12 @@ def install_nginx():
     execute(["apt", "-y", "install", "nginx"])
 
 
+def setup_firewall_for_nginx():
+    execute(["ufw", "allow", "Nginx HTTP"])
+
+
 if __name__ == "__main__":
     cprint("Part 2: Nginx Reverse Proxy", color="blue", newline=True)
 
     install_nginx()
+    setup_firewall_for_nginx()
