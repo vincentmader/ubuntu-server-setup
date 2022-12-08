0546af818bec9635e81baa7efd4bb400c06c4bd8
Vincent C. Mader <vincent.mader@icloud.com>
2022-12-08 23:44:34 +0100

    added nginx installation

--- src/p2_nginx_reverse_proxy/src/main.py
+++ src/p2_nginx_reverse_proxy/src/main.py
@@ -1,4 +1,12 @@
 from utils.cprint import cprint
+from utils.shell import execute
+
+
+def install_nginx():
+    execute(["apt", "install", "nginx"])
+
 
 if __name__ == "__main__":
     cprint("Part 2: Nginx Reverse Proxy", color="blue", newline=True)
+
+    install_nginx()
