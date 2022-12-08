3e6c6c3f2d494c7d3aabd2fbe9fd492fd7474893
Vincent C. Mader <vincent.mader@icloud.com>
2022-12-09 00:09:54 +0100

    added nginx server block creation

--- src/p2_nginx_reverse_proxy/src/main.py
+++ src/p2_nginx_reverse_proxy/src/main.py
@@ -1,5 +1,6 @@
 from utils.cprint import cprint
 from utils.shell import execute
+from utils.shell import get_ip_address
 
 
 def install_nginx():
@@ -11,8 +12,29 @@ def setup_firewall_for_nginx():
     execute(["systemctl", "status", "nginx"])
 
 
+def create_nginx_server_block(domain, ip_address):
+    content = ""
+    content += "server {\n"
+    content += "    listen 80;\n"
+    content += "    listen [::]:80;\n"
+    content += "\n"
+    content += "    server_name {} www.{};\n".format(domain, domain)
+    content += "\n"
+    content += "    location / {\n"
+    content += "        proxy_pass {};\n".format(ip_address)
+    content += "        include proxy_params;\n"
+    content += "    }\n"
+    content += "}"
+    print(content)
+
+
 if __name__ == "__main__":
     cprint("Part 2: Nginx Reverse Proxy", color="blue", newline=True)
 
     install_nginx()
     setup_firewall_for_nginx()
+
+    domain = "sonoapp.de"
+    ip_address = get_ip_address()
+    print(ip_address)
+    nginx_server_block = create_nginx_server_block(domain, ip_address)
