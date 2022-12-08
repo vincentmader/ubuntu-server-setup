b4235c420544542b8b288e9951e028a373f73dd7
Vincent C. Mader <vincent.mader@icloud.com>
2022-12-08 23:31:31 +0100

    setting password setting

--- src/p1_initial_server_setup/src/main.py
+++ src/p1_initial_server_setup/src/main.py
@@ -13,8 +13,13 @@ def grant_root_privileges(user):
     execute(["usermod", "-aG", "sudo", user])
 
 
+def set_password(user):
+    execute(["passwd", user])
+
+
 if __name__ == "__main__":
     cprint("Part 1: Initial Server Setup", color="blue", newline=True)
 
     user = create_new_user()
     grant_root_privileges(user)
+    set_password(user)
